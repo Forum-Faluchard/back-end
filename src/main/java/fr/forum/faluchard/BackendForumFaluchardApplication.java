2a1424a7505067e1d4dfe66082856146d5a9fd35
BossyRomain <romain.bossy.01@gmail.com>
2025-11-29 21:16:37 +0100

    (build) Setup Spring project

---
+++ src/main/java/fr/forum/faluchard/BackendForumFaluchardApplication.java
@@ -0,0 +1,13 @@
+package fr.forum.faluchard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForumFaluchardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForumFaluchardApplication.class, args);
+	}
+
+}
