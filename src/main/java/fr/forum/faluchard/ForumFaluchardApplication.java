1423830ce707c7d3af922b68eca3d83a99e0bee9
BossyRomain <romain.bossy.01@gmail.com>
2025-12-14 16:56:08 +0100

    (build) Setup Spring project

---
+++ src/main/java/fr/forum/faluchard/ForumFaluchardApplication.java
@@ -0,0 +1,13 @@
+package fr.forum.faluchard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumFaluchardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumFaluchardApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO fuser (username, upassword) VALUES ('superAdmin', '0000');
